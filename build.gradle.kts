279e2e36c51a851b0b84c0a30c78ec5f9406695b
Alex-Program <sasha-radchenko.2000@mail.ru>
2020-08-08 14:48:38 +1000

    set jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ val fatJar = task("fatJar", type = Jar::class) {
     manifest {
         attributes["Implementation-Title"] = "Gradle Jar File Example"
         attributes["Implementation-Version"] = version
-        attributes["Main-Class"] = "com.mkyong.DateUtils"
+        attributes["Main-Class"] = "Main"
     }
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
     with(tasks.jar.get() as CopySpec)
